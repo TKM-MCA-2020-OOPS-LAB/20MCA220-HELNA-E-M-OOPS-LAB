b372346c63f325fb0a4d9cd827da2b2620449f0c
HELNAEM2022 <75982355+HELNAEM2022@users.noreply.github.com>
2021-06-09 20:10:08 +0530

    Add files via upload

---
+++ CO-4/Q2/main.java
@@ -0,0 +1,17 @@
+package project;
+import project.arithmetic.operations;
+public class main
+{
+   public static void main(String[] args) 
+	{
+
+		operations obj = new operations();
+		
+		System.out.println("   ARITHMETIC OPERATIONS   ");
+    	obj.addition();
+    	obj.subtraction();
+    	obj.multiplication();
+    	obj.division();
+}
+    
+}
