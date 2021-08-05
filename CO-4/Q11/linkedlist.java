977639fc1b0876c7ffe34ee9550ea53f8461323c
HELNAEM2022 <75982355+HELNAEM2022@users.noreply.github.com>
2021-08-05 23:02:06 +0530

    Add files via upload

---
+++ CO-4/Q11/linkedlist.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+
+public class linkedlist
+{
+    
+    public static void main(String[] args) {
+        int n;
+        String data;
+        LinkedList<String> ll = new LinkedList<String>();
+        System.out.println("Enter the number of data");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        System.out.println("Enter the data");
+        sc.nextLine();
+        for(int i=0;i<n;i++)
+        {
+            data = sc.nextLine();
+            ll.add(data);
+        }
+        System.out.println("LinkedList: "+ll);
+        System.out.println("Removing all the elements....");
+        ll.clear();
+        System.out.println(ll);
+    }
+}
